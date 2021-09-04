36719915181f0b251da97c9be6cb70dac82fc48a
Christoph Heuel <christoph@heuel-web.de>
2021-09-04 23:08:39 +0200

    Add Umlauts

--- notify.py
+++ notify.py
@@ -23,8 +23,8 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
     }
 )
 
-bold_re = re.compile(r'(.*?)(\s*)\*([a-zA-Z0-9 _+/\\-°,;:]+)\*(\s*)(.*)', re.DOTALL)
-italic_re = re.compile(r'(.*?)(\s*)_([a-zA-Z0-9 _+/\\-°]+,;:)_(\s*)(.*)', re.DOTALL)
+bold_re = re.compile(r'(.*?)(\s*)\*([a-zA-ZäÄöÖüÜ0-9 _+/\\-°,;:]+)\*(\s*)(.*)', re.DOTALL)
+italic_re = re.compile(r'(.*?)(\s*)_([a-zA-ZäÄöÖüÜ0-9 _+/\\-°]+,;:)_(\s*)(.*)', re.DOTALL)
 
 bold_trans = "Test".maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
                               "𝗔𝗕𝗖𝗗𝗘𝗙𝗚𝗛𝗜𝗝𝗞𝗟𝗠𝗡𝗢𝗣𝗤𝗥𝗦𝗧𝗨𝗩𝗪𝗫𝗬𝗭𝗮𝗯𝗰𝗱𝗲𝗳𝗴𝗵𝗶𝗷𝗸𝗹𝗺𝗻𝗼𝗽𝗾𝗿𝘀𝘁𝘂𝘃𝘄𝘅𝘆𝘇𝟬𝟭𝟮𝟯𝟰𝟱𝟲𝟳𝟴𝟵")
